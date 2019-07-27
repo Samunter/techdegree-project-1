a72bb4ae79ee8689c6a67d4044ea0d0a4141b782
Samma <46258134+Samunter@users.noreply.github.com>
2019-07-26 17:32:20 -0700

    auto change quote and background color every 5 seconds

--- js/script.js
+++ js/script.js
@@ -4,35 +4,33 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 //create array of quotes
-
 var quotes = [
   {
     quote:
-      '"What would America be like if we loved black people as much as we love black culture?"',
+      'What would America be like if we loved black people as much as we love black culture?',
     source: 'Amandla Stenberg',
     citation: "Don't Cash Crop My Cornrows, YouTube",
     year: '',
     category: 'cultural appropriation'
   },
   {
-    quote:
-      '"Where justice is denied, where poverty is enforced, where ignorance prevails, and where any one class is made to feel that society is in an organized conspiracy to oppress, rob, and degrade them, neither persons nor property will be safe"',
-    source: 'Frederick Douglass',
-    citation: '',
-    year: '1886',
+    quote: 'Healing begins where the wound was made',
+    source: 'Alice Walker',
+    citation: 'The Way Forward Is with a Broken Heart',
+    year: '2000',
     category: 'liberation'
   },
   {
     quote:
-      '"I am an invisible man...I am a man of substance, of flesh and bone, fiber and liquids—and I might even be said to possess a mind. I am invisible, understand, simply because people refuse to see me."',
-    source: 'Ralph Ellison',
-    citation: 'Invisible Man',
-    year: '1952',
+      'Anyone who has ever struggled with poverty knows how extremely expensive it is to be poor.',
+    source: 'James Baldwin',
+    citation: 'Fifth Avenue, Uptown. Esquire',
+    year: '',
     category: 'oppression'
   },
   {
     quote:
-      '"Oppressive language does more than represent violence; it is violence; does more than represent the limits of knowledge; it limits knowledge."',
+      'Oppressive language does more than represent violence; it is violence; does more than represent the limits of knowledge; it limits knowledge.',
     source: 'Toni Morrison',
     citation: 'Nobel lecture',
     year: '1993',
@@ -40,7 +38,7 @@ var quotes = [
   },
   {
     quote:
-      '"Ignorance, allied with power, is the most ferocious enemy justice can have"',
+      'Ignorance, allied with power, is the most ferocious enemy justice can have',
     source: 'James Baldwin',
     citation: 'No Name in the Street',
     year: '1972',
@@ -48,10 +46,17 @@ var quotes = [
   }
 ];
 
-//create function to return random quote from quotes array
+//create array of hex colors
+var colors = ['#b30086', '#b300b3', '#009999', '#0000ff', '#cc99ff'];
 
+//create function to return random quote from quotes array
 function getRandomQuote() {
   return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+//create function to return random hex color from colors array
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * quotes.length)];
 }
 
 //create function to print quote to the screen
@@ -78,9 +83,17 @@ function printQuote() {
 
   htmlString += '</p>';
 
-  //set html of div with id "quote-box" as the html string
+  //set html of div with id "quote-box" as the htmlString
   document.getElementById('quote-box').innerHTML = htmlString;
+
+  //set background and button to random color
+  var randomColor = getRandomColor();
+
+  document.body.style.backgroundColor = randomColor;
+  document.getElementById('loadQuote').style.backgroundColor = randomColor;
 }
+
+window.setInterval(printQuote, 5000);
 
 /***
   When the "Show another quote" button is clicked, the event listener 
@@ -92,5 +105,3 @@ function printQuote() {
 document
   .getElementById('loadQuote')
   .addEventListener('click', printQuote, false);
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
